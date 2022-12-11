553f9da6c1e8c6c0ceee2ee4db729feba51951b1
guilherme-humberto <gbreu@outlook.com>
2022-12-11 15:04:01 -0300

    feature: criação página cadastro de alunos

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import InitialView from '../views/Initial.vue'
+import InitialView from '../views/Home/main.vue'
+import StudentInitialView from '../views/Student/main.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'initial',
       component: InitialView
+    },
+    {
+      path: '/student',
+      name: 'student-initial',
+      component: StudentInitialView
     }
   ]
 })
---
+++ src/views/Home/main.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+import { RouterLink } from 'vue-router'
+import BaseButton from '../../components/atoms/BaseButton.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="wrapper">
+      <div class="github">
+        <Icon icon="ant-design:github-filled" />
+        <a
+          href="https://github.com/Guilherme-Humberto/theacher-app"
+          target="_blank"
+          >Visitar projeto</a
+        >
+      </div>
+      <h1 class="title">Sua plataforma de estudos online</h1>
+      <p class="subtitle">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor.
+      </p>
+      <div class="btns">
+        <RouterLink to="/student">
+          <BaseButton type="submit" typeStyle="fill" text="Estudar na theacher" />
+        </RouterLink>
+        <RouterLink to="/teacher">
+          <BaseButton type="submit" typeStyle="outline" text="Dar aulas na theacher" />
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: url('/icons/ilustration.svg');
+  background-size: 100% 70%;
+  background-repeat: no-repeat;
+  background-position: right center;
+  width: 100%;
+}
+
+.wrapper {
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+}
+
+.github {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 30px;
+}
+
+.github svg {
+  font-size: 20px;
+}
+
+.github a {
+  text-decoration: none;
+  color: var(--color-text);
+  font-family: var(--font-secondary);
+}
+
+.title {
+  font-size: 60px;
+  letter-spacing: -9%;
+  font-weight: bold;
+  line-height: 75px;
+  max-width: 800px;
+}
+
+.subtitle {
+  font-size: 18px;
+  margin: 30px 0;
+  max-width: 455px;
+}
+
+.btns {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+</style>
